d71aa93edb93020eff763854d2190ccd009755de
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-22 03:44:21 -0300

    Atualização no DB

--- src/db/database-connection.js
+++ src/db/database-connection.js
@@ -5,7 +5,7 @@ const admSecret = process.env.SECRET_ADM
 const keySecret = process.env.SECRET_KEY
 
 const dbUrl = `mongodb+srv://${admSecret}:${keySecret}@cluster0.3rntpik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-const dbName = 'mongodb-arquitetura-mvc'
+const dbName = 'mongodb-integrar-com-frontend'
 const client = new MongoClient(dbUrl)
 
 async function connectToDatabase() {
